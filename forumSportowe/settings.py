cc7ebc1771d41ac2e58ea3d665a1750c51fa29c5
Rafał Stawecki <rafal.stawecki@gmail.com>
2021-04-18 14:13:33 +0200

    added database postgresql

--- forumSportowe/settings.py
+++ forumSportowe/settings.py
@@ -87,6 +87,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 
 # Password validation
